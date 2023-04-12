6a5e6d23479386ae093e9909c22fc7669455cabb
CCaonguyen <caonguyen2262003@gmail.com>
2023-04-12 08:21:18 +0700

    size

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -147,11 +147,7 @@
                                    
                                 </div>
                                 <div class="col-sm-3 col-xl-3 col-md-3">
-                                    <a href="#">
-                                        <div class="card" style="width: 100%; ">
-                                            <img src="https://cdn.tgdd.vn/Products/Images/42/245545/iPhone-14-plus-thumb-den-600x600.jpg" class="card-img-top" >
-                                        </div>
-                                    </a>
+                                  
                                     
                                 </div>
                                 <div class="col-sm-3 col-xl-3 col-md-3">
