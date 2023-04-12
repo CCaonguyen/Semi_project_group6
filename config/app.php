ff3373502a03a1261ed14718d4572174988346aa
hungldbhaf <102125714+hungldbhaf@users.noreply.github.com>
2023-04-12 08:35:34 +0700

    update home Css

--- config/app.php
+++ config/app.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
-    | framework needs to p-lace the application's name in a notification or
+    | framework need0s to p-lace the application's name in a notification or
     | any other location as required by the application or its packages.
     |
     */
